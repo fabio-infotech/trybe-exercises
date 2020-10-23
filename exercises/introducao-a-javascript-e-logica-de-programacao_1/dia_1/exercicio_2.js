3b338fcc7a770f52273355390ca24d3ef6ee17cc
Fabio Ferreira <fabio35547@gmail.com>
2020-10-23 18:15:18 -0300

    Exercicio 2 - Concluido

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/dia_1/exercicio_2.js
@@ -0,0 +1,10 @@
+let a = 10;
+let b = 20;
+
+if (a >= b){
+    console.log("O maior número entre " + a + " e " + b + " é: " + a);
+}
+else {
+    console.log("O maior número entre " + a + " e " + b + " é: " + b);
+}
+
