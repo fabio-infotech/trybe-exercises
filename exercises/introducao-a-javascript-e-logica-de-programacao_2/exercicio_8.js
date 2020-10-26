8952770a64f7a1de3ff627f8f91785aa80f0ac6d
Fabio Ferreira <fabio35547@gmail.com>
2020-10-26 18:46:51 -0300

    Exercicio 8 - resolvido

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio_8.js
@@ -0,0 +1,10 @@
+let numbers = [];
+const menorValor = 1;
+const maiorValor = 25;
+
+for(let contador = menorValor; contador <= maiorValor; contador++)
+{
+    numbers.push(contador);
+}
+
+console.log(`Novo array: ${numbers}`);
