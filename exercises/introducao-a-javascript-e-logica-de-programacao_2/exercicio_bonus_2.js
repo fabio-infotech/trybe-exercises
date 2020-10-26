fecbccaf1cec0779fe786142b2a7cdcf95b85358
Fabio Ferreira <fabio35547@gmail.com>
2020-10-26 19:24:26 -0300

    Exercicio bonus 2 - Incluindo nova forma de ordenacao

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio_bonus_2.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio_bonus_2.js
@@ -3,3 +3,17 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 numbers.sort((a, b) => b - a);
 
 console.log(`Array em ordem decrescente: ${numbers}`);
+
+//OUTRA FORMA DE ORDENAÇÃO
+
+for (let i = 1; i < numbers.length; i++) {
+    for (let j = 0; j < i ; j++){
+      if (numbers[i] > numbers[j]) {
+        let position = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = position;
+      }
+    }
+  }
+  
+  console.log(`Array em ordem decrescente B: ${numbers}`);
