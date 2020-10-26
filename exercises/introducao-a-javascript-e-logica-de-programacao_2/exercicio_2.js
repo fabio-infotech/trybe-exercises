367e99e907f7e4efe089bb1a4ecc06b4944d0ce2
Fabio Ferreira <fabio35547@gmail.com>
2020-10-26 18:22:55 -0300

    Exercicio 2 - resolvido

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio_2.js
@@ -0,0 +1,9 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for(let indice = 0; indice < numbers.length; indice++)
+{
+    soma = soma + numbers[indice];
+}
+
+console.log(`A soma dos valores do vetor é: ${soma}`);
