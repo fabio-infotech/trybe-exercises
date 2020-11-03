a690e0ef616688499a8ce39f4b4ce071a519a074
Fabio Ferreira <fabio35547@gmail.com>
2020-11-03 18:34:36 -0300

    Exercicio 2 - concluido

--- exercises/javascript-dom-eventos-e-web-storage_2/script.js
+++ exercises/javascript-dom-eventos-e-web-storage_2/script.js
@@ -1,6 +1,12 @@
 // Exercício 1
+//Criação do elemento html
 let h1 = document.createElement('h1');
-
-h1.innerHTML = "Exercício 5.2 - JavaScript DOM";
-
+//Definindo propriedades do elemento html
+h1.innerHTML = 'Exercício 5.2 - JavaScript DOM';
+//Inclindo o elemento na página html
 document.body.appendChild(h1);
+
+// Exercicio 2
+let div = document.createElement('div');
+div.className = 'main-container';
+document.body.appendChild(div);
