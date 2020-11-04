39f44d7749cc2d7c81402e9bfbc2e51550d100d2
Fabio Ferreira <fabio35547@gmail.com>
2020-11-04 18:49:49 -0300

    Incluindo conteudo da aula ao vivo

---
+++ exercises/javascript-dom-eventos-e-web-storage_3/aula_ao_vivo/script.js
@@ -0,0 +1,32 @@
+window.onload = function () {
+  let button = document.getElementById('start-race-btn');
+  let car1 = document.querySelector('.car1');
+  let car2 = document.querySelector('.car2');
+
+  car1.style.marginLeft = '0px';
+  car2.style.marginLeft = '0px';
+
+  //1. qual o tipo de evento que devo escutar?
+  //2. o que eu faço quando esse evento ocorrer?
+  button.addEventListener('click', function () {
+    
+    car1.style.marginLeft = parseInt(car1.style.marginLeft) + (Math.random() * 100) + 'px';
+    car2.style.marginLeft = parseInt(car2.style.marginLeft) + (Math.random() * 100) + 'px';
+
+    if (parseInt(car1.style.marginLeft) > window.innerWidth) {
+      alert('Carro VERMELHO ganhou!');
+      resetCars(car1, car2);
+    };
+
+    if (parseInt(car2.style.marginLeft) > window.innerWidth) {
+      alert('Carro VERDE ganhou!');
+      resetCars(car1, car2);
+    };
+
+  });
+};
+
+function resetCars (car1, car2) {
+  car1.style.marginLeft = '0px';
+  car2.style.marginLeft = '0px';
+};
