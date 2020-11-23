5ec218c2158f05dfb42a134a7c7aa9ef05993835
fabio-infotech <fabio35547@gmail.com>
2020-11-23 18:47:04 -0300

    Exercicio parte 1 - Requisito 2

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_1/script_parte_1.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_1/script_parte_1.js
@@ -9,3 +9,9 @@ const testingScope = (escopo) => {
 }
 
 console.log(testingScope(true));
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+oddsAndEvens.sort((a, b) => a - b);
+
+console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`);
