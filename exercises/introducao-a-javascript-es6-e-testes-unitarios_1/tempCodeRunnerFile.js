b2d55b6cc235265bf2f070073e97073b1cd61518
fabio-infotech <fabio35547@gmail.com>
2020-11-23 22:46:56 -0300

    Exercicio parte 2 - Requisito 4

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_1/tempCodeRunnerFile.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_1/tempCodeRunnerFile.js
@@ -1,3 +1,18 @@
 const x = (frase, strReplace) => frase.replace('x', strReplace);
 
 console.log(x('Tryber x aqui!', 'Bebeto'));
+
+const arrayHabilidades = ['Git', 'HTML', 'JavaScript', 'CSS', 'Unix'];
+
+const y = (frase) =>   
+  `${frase}
+  Minhas cinco habilidades são:
+  - ${arrayHabilidades.sort()[0]};
+  - ${arrayHabilidades.sort()[1]};
+  - ${arrayHabilidades.sort()[2]};
+  - ${arrayHabilidades.sort()[3]};
+  - ${arrayHabilidades.sort()[4]};
+  #goTrybe.
+  `
+
+console.log(y(x('Tryber x aqui!', 'Bebeto')));
