3c18694930734522fefe552b881022310aba4c25
Fabio Ferreira <fabio35547@gmail.com>
2021-01-04 19:09:40 -0300

    Parte 1 - Exercicio 5

--- exercises/testes-automatizados-com-jest_1/parte1_exercicio_1.test.js
+++ exercises/testes-automatizados-com-jest_1/parte1_exercicio_1.test.js
@@ -17,4 +17,8 @@ describe('Exercícios de Testes Unitário 1-5', () => {
   test('Teste se a função sum lança um erro quando os parametros são 4 e "5" (string 5)', () => {
     expect(() => { sum(4, '5') }).toThrow();
   });
+  // Exercício 5
+  test('Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")', () => {
+    expect(() => { sum(4, '5') }).toThrowError(new Error('parameters must be numbers'));
+  });
 });
