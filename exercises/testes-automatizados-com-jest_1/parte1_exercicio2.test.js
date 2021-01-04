59d1691f1ffbe489a79580310e7940875b0eb3c7
Fabio Ferreira <fabio35547@gmail.com>
2021-01-04 19:28:28 -0300

    Parte 2 - Exercicio 2

--- exercises/testes-automatizados-com-jest_1/parte1_exercicio2.test.js
+++ exercises/testes-automatizados-com-jest_1/parte1_exercicio2.test.js
@@ -4,5 +4,9 @@ describe('Exercícios de Testes Unitário 2', () => {
   // Exercício 2.1
   test('A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item , caso ele exista no array', () => {
     expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
-  });  
+  });
+  // Exercício 2.2
+  test('Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  });
 });
