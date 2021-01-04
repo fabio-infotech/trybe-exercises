dcf07cb5f82627159dbf2ddd450df4ec8773dc46
Fabio Ferreira <fabio35547@gmail.com>
2021-01-04 19:08:12 -0300

    Parte 1 - Exercicio 4

--- exercises/testes-automatizados-com-jest_1/parte1_exercicio_1.test.js
+++ exercises/testes-automatizados-com-jest_1/parte1_exercicio_1.test.js
@@ -3,14 +3,18 @@ const sum = require('./parte1_exercicio_1');
 describe('Exercícios de Testes Unitário 1-5', () => {
   // Exercício 1
   test('A função sum(a, b) retorna a soma do parâmetro a com o b', () => {
-    expect(sum(2, 2)).toBe(4);
+    expect(typeof sum).toBe('function');
   });
   // Exercício 2
   test('Teste se o retorno de sum(4, 5) é 9', () => {
     expect(sum(4, 5)).toBe(9);
   });
   // Exercício 3
-  test('TTeste se o retorno de sum(0, 0) é 0', () => {
+  test('Teste se o retorno de sum(0, 0) é 0', () => {
     expect(sum(0, 0)).toBe(0);
+  });
+  // Exercício 4
+  test('Teste se a função sum lança um erro quando os parametros são 4 e "5" (string 5)', () => {
+    expect(() => { sum(4, '5') }).toThrow();
   });
 });
