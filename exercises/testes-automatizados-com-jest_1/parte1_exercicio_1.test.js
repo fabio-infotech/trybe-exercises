fa7f563bff21de96f507454e5b5d42835b123387
Fabio Ferreira <fabio35547@gmail.com>
2021-01-04 19:00:15 -0300

    Parte 1 - Exercicio 2

--- exercises/testes-automatizados-com-jest_1/parte1_exercicio_1.test.js
+++ exercises/testes-automatizados-com-jest_1/parte1_exercicio_1.test.js
@@ -1,6 +1,12 @@
-const { test, expect } = require('@jest/globals');
 const sum = require('./parte1_exercicio_1');
 
-test('A função sum(a, b) retorna a soma do parâmetro a com o b', () => {
-  expect(sum(2, 2)).toBe(4);
+describe('Exercícios de Testes Unitário 1-5', () => {
+  // Exercício 1
+  test('A função sum(a, b) retorna a soma do parâmetro a com o b', () => {
+    expect(sum(2, 2)).toBe(4);
+  });
+  // Exercício 2
+  test('Teste se o retorno de sum(4, 5) é 9', () => {
+    expect(sum(4, 5)).toBe(9);
+  });
 });
