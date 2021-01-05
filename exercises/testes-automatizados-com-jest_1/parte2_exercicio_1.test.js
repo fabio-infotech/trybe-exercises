4a5da6fcbd92fc60a83eb9d0603bdc48721f93c7
Fabio Ferreira <fabio35547@gmail.com>
2021-01-05 01:17:26 -0300

    Parte 2 - Exercicio 1.3

--- exercises/testes-automatizados-com-jest_1/parte2_exercicio_1.test.js
+++ exercises/testes-automatizados-com-jest_1/parte2_exercicio_1.test.js
@@ -24,4 +24,20 @@ describe('Testa a função encode e decode', () => {
   it('converte a vogal "u" no número 5', () => {
     expect(encode('nu')).toEqual('n5');
   });
+  // Exercício 1.4
+  it('converte apenas o número 1 na vogal a', () => {
+    expect(decode('1n1')).toEqual('ana');
+  });
+  it('converte apenas o número 2 na vogal e', () => {
+    expect(decode('2l2')).toEqual('ele');
+  });
+  it('converte apenas o número 3 na vogal i', () => {
+    expect(decode('x3x3')).toEqual('xixi');
+  });
+  it('converte apenas o número 4 na vogal o', () => {
+    expect(decode('4v4')).toEqual('ovo');
+  });
+  it('converte apenas o número 5 na vogal u', () => {
+    expect(decode('n5')).toEqual('nu');
+  });
 });
