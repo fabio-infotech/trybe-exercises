53b5ee5d3a604abedaa828e2eac5cb44f7722bc9
Fabio Ferreira <fabio35547@gmail.com>
2021-01-05 00:59:53 -0300

    Parte 1 - Exercicio 4.1

--- exercises/testes-automatizados-com-jest_1/parte1_exercicio4.js
+++ exercises/testes-automatizados-com-jest_1/parte1_exercicio4.js
@@ -1,3 +1,8 @@
-const myRemove = (arr, item) => arr.filter((element) => element !== item)
-
-module.exports = myRemove;
+const myFizzBuzz = num => {
+  if (typeof num !== 'number') return false;
+  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+  if (num % 3 === 0) return 'fizz';
+  if (num % 5 === 0) return 'buzz';
+  return num;
+}
+module.exports = myFizzBuzz;
--- exercises/testes-automatizados-com-jest_1/parte1_exercicio4.test.js
+++ exercises/testes-automatizados-com-jest_1/parte1_exercicio4.test.js
@@ -1,22 +1,8 @@
-const myRemove = require('./parte1_exercicio2');
+const myFizzBuzz = require('./parte1_exercicio4');
 
-describe('Exercícios de Testes Unitário 2', () => {
-  // Exercício 2.3
-  test('Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado', () => {
-    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
-  });
-  // Exercício 2.4
-  test('Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
-    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
-  });
-  // Exercício 2.5
-  test('Faça uma chamada para a função myRemove e verifique se o array passado por parâmetro não sofreu alterações', () => {
-    const arr = [1, 2, 3, 4];
-    myRemove(arr, 1);
-    expect(arr).toEqual([1, 2, 3, 4]);
-  });
-  // Exercício 2.6
-  test('Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado', () => {
-    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
-  });
+describe('Exercícios de Testes Unitário 4', () => {
+  // Exercício 4.5
+  test('Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado', () => {
+    expect(myFizzBuzz(15)).toBe('fizzbuzz');
+  });  
 });
