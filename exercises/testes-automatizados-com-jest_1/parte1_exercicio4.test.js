2e962140319d1a71ccd0a700747307d85f716eb4
Fabio Ferreira <fabio35547@gmail.com>
2021-01-05 01:02:43 -0300

    Parte 1 - Exercicio 4.4

--- exercises/testes-automatizados-com-jest_1/parte1_exercicio4.test.js
+++ exercises/testes-automatizados-com-jest_1/parte1_exercicio4.test.js
@@ -13,4 +13,8 @@ describe('Exercícios de Testes Unitário 4', () => {
   test('Faça uma chamada com um número divisível por 5 e verifique se o retorno é o esperado', () => {
     expect(myFizzBuzz(25)).toBe('buzz');
   });
+  // Exercício 4.8
+  test('Faça uma chamada com um número que não é divisível por 3 ou 5 e verifique se o retorno é o esperado', () => {
+    expect(myFizzBuzz(17)).toBe(17);
+  });
 });
