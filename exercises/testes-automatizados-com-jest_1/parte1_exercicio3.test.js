d156e9b91ce403ca815279e692696801bc64aa06
Fabio Ferreira <fabio35547@gmail.com>
2021-01-05 00:54:50 -0300

    Parte 1 - Exercicio 3.2

--- exercises/testes-automatizados-com-jest_1/parte1_exercicio3.test.js
+++ exercises/testes-automatizados-com-jest_1/parte1_exercicio3.test.js
@@ -5,4 +5,8 @@ describe('Exercícios de Testes Unitário 3', () => {
   test('Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) retorna o array esperado', () => {
     expect(myRemoveWithoutCopy([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
   });
+  // Exercício 3.5
+  test('Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
+    expect(myRemoveWithoutCopy([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  });
 });
