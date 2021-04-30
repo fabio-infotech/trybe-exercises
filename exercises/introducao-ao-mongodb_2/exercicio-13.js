919022828e4f07d9d7a63c23a80e503ade2c9b04
Fabio Ferreira <fabio35547@gmail.com>
2021-04-29 21:48:43 -0300

    exercicio-13

---
+++ exercises/introducao-ao-mongodb_2/exercicio-13.js
@@ -0,0 +1,2 @@
+// Selecione todos os super-heróis que não sejam humanos ou que não sejam maiores do que 1.80m
+db.superheroes.find({ $nor: [{ race: "Human" }, { "aspects.height": { $gt: 180 } }] }).pretty();
