4da3325f43d73bd9c790b3f5be249ddf7b4d2c95
Fabio Ferreira <fabio35547@gmail.com>
2021-05-05 18:35:28 -0300

    Exercicio 8

--- exercises/mongodb-updates-simples-e-complexos_2/exercicio-8.js
+++ exercises/mongodb-updates-simples-e-complexos_2/exercicio-8.js
@@ -12,3 +12,18 @@
   "actor": "Daniel Stern"
 }
 */ 
+db.movies.updateOne
+(
+  { title: "Home Alone" },
+  {
+    $push: {
+      cast: {
+        $each: [
+          { "actor": "Macaulay Culkin", "character": "Kevin" },
+          { "actor": "Joe Pesci", "character": "Harry" },
+          { "actor": "Daniel Stern" }
+        ],
+      },
+    },
+  },
+);
