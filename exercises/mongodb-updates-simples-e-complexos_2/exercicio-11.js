33ed4fd7ab2400c5e2688dac1fa8e596e63178eb
Fabio Ferreira <fabio35547@gmail.com>
2021-05-05 19:26:31 -0300

    Exercicio 11

--- exercises/mongodb-updates-simples-e-complexos_2/exercicio-11.js
+++ exercises/mongodb-updates-simples-e-complexos_2/exercicio-11.js
@@ -4,3 +4,41 @@ Adicione o campo actor , que deve ser um array com o valor Christian Bale , ao a
 Adicione o campo actor , que deve ser um array com o valor Michael Caine , ao array de cast em que o campo character seja igual a Alfred ;
 Adicione o campo actor , que deve ser um array com o valor Heath Ledger , ao array de cast em que o campo character seja igual a Coringa
 */
+db.movies.updateOne
+(
+  { title: "Batman" },
+  {
+    $push: { "cast.$[elemento].actor": "Christian Bale" }
+  },
+  {
+    arrayFilters: [
+      { "elemento.character": "Batman" }
+    ],
+  },
+);
+
+db.movies.updateOne
+(
+  { title: "Batman" },
+  {
+    $push: { "cast.$[elemento].actor": "Michael Caine" }
+  },
+  {
+    arrayFilters: [
+      { "elemento.character": "Alfred" }
+    ],
+  },
+);
+
+db.movies.updateOne
+(
+  { title: "Batman" },
+  {
+    $push: { "cast.$[elemento].actor": "Heath Ledger" }
+  },
+  {
+    arrayFilters: [
+      { "elemento.character": "Coringa" },
+    ],
+  },
+);
