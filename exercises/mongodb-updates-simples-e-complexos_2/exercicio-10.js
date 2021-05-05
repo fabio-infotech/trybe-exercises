d727befb1c38ba7077f4c31536774bf40594d063
Fabio Ferreira <fabio35547@gmail.com>
2021-05-05 18:54:45 -0300

    Exercicio 10

--- exercises/mongodb-updates-simples-e-complexos_2/exercicio-10.js
+++ exercises/mongodb-updates-simples-e-complexos_2/exercicio-10.js
@@ -10,3 +10,18 @@
   "character": "Coringa"
 }
 */
+db.movies.updateOne
+(
+  { title: "Batman" },
+  {
+    $push: {
+      cast: {
+        $each: [
+          { "character": "Batman" },
+          { "character": "Alfred" },
+          { "character": "Coringa" }
+        ],
+      },
+    },
+  },
+);
