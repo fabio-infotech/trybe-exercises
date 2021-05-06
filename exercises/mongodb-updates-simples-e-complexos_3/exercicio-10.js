5f7c2d99b388a08867555a1429522c53453a60fb
Fabio Ferreira <fabio35547@gmail.com>
2021-05-06 19:56:49 -0300

    Exercicio 10

--- exercises/mongodb-updates-simples-e-complexos_3/exercicio-10.js
+++ exercises/mongodb-updates-simples-e-complexos_3/exercicio-10.js
@@ -1,1 +1,12 @@
 // Retorne somente o título de todos os filmes com dois elementos no array category
+db.movies.find(
+  {
+    category: {
+      $size: 2
+    },
+  },
+  {
+    _id: -1,
+    title: 1,
+  }
+);
