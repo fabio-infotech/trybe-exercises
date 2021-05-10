1519a5f53d3b129180910748cd9a3d1f99c2bfcf
Fabio Ferreira <fabio35547@gmail.com>
2021-05-10 19:12:36 -0300

    Course exercicio group 2

--- exercises/mongodb-aggregation-framework_1/course-exercicio-group-2.js
+++ exercises/mongodb-aggregation-framework_1/course-exercicio-group-2.js
@@ -1,1 +1,12 @@
 // Selecione o valor total das transações em cada banco e quantas são
+db.transactions.aggregate(
+  [
+    {
+      $group: {
+        _id: "$bank",
+        totalSales: { $sum: "$value" },
+        count: { $sum: 1 },
+      },
+    },
+  ],
+);
