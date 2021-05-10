ff36316dc9f01b1258b455d4f4b1c2fb661db6db
Fabio Ferreira <fabio35547@gmail.com>
2021-05-10 19:17:59 -0300

    Course exercicio lookup 1

---
+++ exercises/mongodb-aggregation-framework_1/course-exercicio-lookup-1.js
@@ -0,0 +1,13 @@
+// Selecione todos os clientes com as suas respectivas transações feitas
+db.clients.aggregate(
+  [
+    {
+      $lookup: {
+        from: "transactions",
+        localField: "name",
+        foreignField: "from",
+        as: "transactions",
+      },
+    },
+  ],
+).pretty();
