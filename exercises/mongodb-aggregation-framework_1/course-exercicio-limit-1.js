1ffab7717590fd92059dfbac7cf633391673a945
Fabio Ferreira <fabio35547@gmail.com>
2021-05-10 19:07:22 -0300

    Course exercicio limit 1

--- exercises/mongodb-aggregation-framework_1/course-exercicio-limit-1.js
+++ exercises/mongodb-aggregation-framework_1/course-exercicio-limit-1.js
@@ -1,1 +1,8 @@
 // Selecione todas as transações feitas pelo cliente chamado "Dave America"
+db.transactions.aggregate(
+  [
+    { 
+      $match: { from: "Dave America" } 
+    }
+  ]
+).pretty();
