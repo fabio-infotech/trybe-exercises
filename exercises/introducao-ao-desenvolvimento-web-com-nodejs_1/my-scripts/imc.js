6a2c54e6873833af4ec7197da31705e7fe2466f3
Fabio Ferreira <fabio35547@gmail.com>
2021-05-14 19:30:48 -0300

    Exercicio 3 e 4

--- exercises/introducao-ao-desenvolvimento-web-com-nodejs_1/my-scripts/imc.js
+++ exercises/introducao-ao-desenvolvimento-web-com-nodejs_1/my-scripts/imc.js
@@ -1,5 +1,10 @@
+const readLine = require('readline-sync');
+
+const inputPeso = readLine.questionFloat('Qual seu peso?');
+const inputAltura = readLine.questionFloat('Qual sua altura?');
+
 function calculaIMC(peso, altura) {
-  return (peso / (altura ** 2));
+  return (peso / (altura ** 2)).toFixed(2);
 }
 
-console.log(calculaIMC(79, 1.81));
+console.log('IMC = %s', calculaIMC(inputPeso, inputAltura));
