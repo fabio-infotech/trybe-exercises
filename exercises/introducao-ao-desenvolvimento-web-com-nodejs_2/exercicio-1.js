f42ef95004c42ac2b30fd229145d77c962024372
Fabio Ferreira <fabio35547@gmail.com>
2021-05-17 18:20:19 -0300

    Exercicio 1

--- exercises/introducao-ao-desenvolvimento-web-com-nodejs_2/exercicio-1.js
+++ exercises/introducao-ao-desenvolvimento-web-com-nodejs_2/exercicio-1.js
@@ -5,3 +5,22 @@ Crie uma função que recebe três parâmetros retorna uma Promise .
   Caso o resultado seja menor que 50, rejeite a Promise com o motivo "Valor muito baixo"
   Caso o resultado seja maior que 50, resolva a Promise com o valor obtido.
 */
+function calcularNumeros(a, b, c) {
+  return new Promise((resolve, reject) => {
+    if (isNaN(a) || isNaN(b) || isNaN(c)) {
+      reject(Error('Informe apenas números'));
+    }
+
+    const result = ((a + b) * c);
+
+    if (result <= 50) {
+      reject(Error('Valor muito baixo'));
+    }
+
+    resolve(result);
+  });
+}
+
+calcularNumeros(100, 4, 6)
+  .then(result => console.log(`sucesso: ${ result }`))
+  .catch(err => console.log(`erro: ${ err.message }`));
