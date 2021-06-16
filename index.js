2abdb5fe9fcd110346f796abbf3dc9a3b7fa401f
Fabio Ferreira <fabio35547@gmail.com>
2021-06-16 19:22:16 -0300

    Exercicio 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Está vivo');
+  res.send(process.env.ENVIROMENT);
 });
 
 const PORT = process.env.PORT || 3000;
