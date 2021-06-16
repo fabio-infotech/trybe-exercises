d1a0a0759232477bae2c4ddd8ec899e03c19baf2
Fabio Ferreira <fabio35547@gmail.com>
2021-06-16 19:29:18 -0300

    Exercicio 3 - Ajuste Procfile

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.send(process.env.ENVIROMENT);
 });
 
--- indexProcfile.js
+++ indexProcfile.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send(process.env.ENVIROMENT);
+app.get('/', (_req, res) => {
+  res.send('Procfile funciona mesmo!');
 });
 
 const PORT = process.env.PORT || 3000;
