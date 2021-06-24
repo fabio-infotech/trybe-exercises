7495d4169556979989d8a47d1ad0e7e417a6b3d7
Fabio Ferreira <fabio35547@gmail.com>
2021-06-24 19:48:44 -0300

    Exercicio resolvido

---
+++ exercises/arquitetura-solid-e-orm_2/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const booksController = require('./src/api/controller/booksController');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.get('/books', booksController.getAll);
+app.get('/book/:id', booksController.getById);
+app.post('/book', booksController.createNew);
+app.post('/book/:id', booksController.updateById);
+app.delete('/book/:id', booksController.deleteById);
+
+app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
