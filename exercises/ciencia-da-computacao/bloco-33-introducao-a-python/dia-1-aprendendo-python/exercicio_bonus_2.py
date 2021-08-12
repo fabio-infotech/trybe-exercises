3cdc53c678e21c08140844f0e509984f0a51518e
Fabio Ferreira <fabio35547@gmail.com>
2021-08-12 20:54:16 -0300

    Exercicio bonus 2

---
+++ exercises/ciencia-da-computacao/bloco-33-introducao-a-python/dia-1-aprendendo-python/exercicio_bonus_2.py
@@ -0,0 +1,16 @@
+# Faça um programa que, dado um valor n qualquer, tal que n > 1, imprima na tela um triângulo retângulo com n asteriscos de base. Por exemplo, para n=5 o triângulo terá 5 asteriscos na base:
+
+'''
+n = 5
+*
+**
+***
+****
+*****
+'''
+
+def draw_triangle(num):
+   for row in range(1, num + 1):
+      print(row * '*')
+
+print(draw_triangle(5))
