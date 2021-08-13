713841e7c6d83320fe1895e21c4840da83efae0e
Fabio Ferreira <fabio35547@gmail.com>
2021-08-13 19:25:49 -0300

    Exercicio 1 - Ajuste

--- exercises/ciencia-da-computacao/bloco-33-introducao-a-python/dia-2-entrada-e-saida-de-dados/exercicio_1.py
+++ exercises/ciencia-da-computacao/bloco-33-introducao-a-python/dia-2-entrada-e-saida-de-dados/exercicio_1.py
@@ -9,7 +9,12 @@ PE
 P
 '''
 
-NAME = input("Insira seu nome: ")
+def vertical_inverted_ladder(word):
+    for removed_letters in range(len(word)):
+        for index in range(len(word) - removed_letters):
+            print(word[index], end="")
+        print()
 
-for letter in NAME:
-  print(letter)
+if __name__ == "__main__":
+    name = input("Digite um nome: ")
+    vertical_inverted_ladder(name)
