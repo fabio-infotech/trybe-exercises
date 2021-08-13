33ded7c68da1a4c53856065e97a9d93a270b28bb
Fabio Ferreira <fabio35547@gmail.com>
2021-08-13 19:20:59 -0300

    Exercicio 1

---
+++ exercises/ciencia-da-computacao/bloco-33-introducao-a-python/dia-2-entrada-e-saida-de-dados/exercicio_1.py
@@ -0,0 +1,15 @@
+# Faça um programa que receba um nome e imprima o mesmo na vertical em escada invertida. Exemplo:
+'''
+PEDRO
+
+PEDRO
+PEDR
+PED
+PE
+P
+'''
+
+NAME = input("Insira seu nome: ")
+
+for letter in NAME:
+  print(letter)
